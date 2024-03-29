e86c1c64a5355313b8bcdbbb9156450643f9f341
Richard King <32266266+rjking1@users.noreply.github.com>
2024-03-29 15:06:30 +1100

    add flow calcs etc

--- src/Diagram.svelte
+++ src/Diagram.svelte
@@ -79,9 +79,9 @@
 
   <Pylon gen={snowy2Gen} />
   <EwPower gen={snowy2Gen} />
-  <Gen name="Snowy 2.0" size="2000MW" gen={snowy2Gen} />
+  <Gen name="Snowy 2.0" size="2000MW" gen={snowy2Gen} turbineSize=340 turbines=6 head=700 maxFlow=6*50 />  
   <Empty />
-  <Gen name="Tumut 1" size="330MW" gen={tumut1Gen} />
+  <Gen name="Tumut 1" size="330MW" gen={tumut1Gen} turbineSize=82 turbines=4 head=292 maxFlow=119 />
   <EPower gen={tumut1Gen} />
   <Empty />
 
@@ -97,7 +97,7 @@
   <Empty />
   <Dam name="Talbingo" size="921,400ML" />
   <WArrow gen={tumut2Gen} />
-  <Gen name="Tumut 2" size="286MW" gen={tumut2Gen} />
+  <Gen name="Tumut 2" size="286MW" gen={tumut2Gen} turbineSize=71 turbines=4 head=262 maxFlow=119 />
   <EPower gen={tumut2Gen} />
   <Empty />
 
@@ -111,7 +111,7 @@
 
   <Pylon gen={tumut3Gen} />
   <EwPower gen={tumut3Gen} />
-  <Gen name="Tumut 3" size="1,800MW" gen={tumut3Gen} />
+  <Gen name="Tumut 3" size="1,800MW (600 pumping)" gen={tumut3Gen} turbineSize=300 turbines=6 head=150 maxFlow=6*188 />
   <Empty />
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={doRefresh}>Refresh</button>
   <Empty />
@@ -146,7 +146,7 @@
 
   <Empty />
   <Empty />
-  <Gen name="Blowering" size="80MW" gen={bloweringGen} />
+  <Gen name="Blowering" size="80MW" gen={bloweringGen} turbineSize=80 turbines=1 head=87 maxFlow=90/>
   <EPower gen={bloweringGen} />
   <Pylon gen={bloweringGen} />
   <Empty />
--- src/Gen.svelte
+++ src/Gen.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
-  export let name: string;
-  export let size: string;
-  export let gen: number;
+  import { Popover } from "flowbite-svelte";
 
-  $:  opp = gen == 0 ? 0.2 : 1;
-  $:  cls = gen == 0 ? '' : gen > 0 ? 'cc' : 'acc';
+  export let name: string;
+  export let gen: number; // current
+  export let size: string;
+  export let head: number = 150;  //eg Tumut3 150m
+  export let turbines: number = 6; // 6
+  export let turbineSize: number = 300; // 300 MW
+  export let maxFlow: number = 188*6; // 188*6 m^3/s for all turbines -- recheck!!!!
+
+  // genMW = head(m) * flow(m^3/s) * eff,say 0.93 * g  / 1000
+  // eg tumut 2 262*119*0.93*9.8 / 1000 = ~280  ; flow = gen * 1000 / (head * eff * g)
+
+  const eff = 0.93
+  const g = 9.8
+
+  $:  calcFlow = Math.trunc(gen * 1000 / (head * eff * g)); // cumecs= m^3/s
+  $:  calcFlowML = Math.trunc(calcFlow * 1000 * 3600 / 1000000); // ML per hour
+
+  $: opp = gen == 0 ? 0.2 : 1;
+  $: cls = gen == 0 ? "" : gen > 0 ? "cc" : "acc";
 
   // console.log(name, gen, cls);
+  const id = Math.trunc(Math.random() * 1000);
 </script>
 
 <div class="side-by-side">
   <div class={cls}>
     <!-- <img src="./assets/noun-hydro-power-6247453.svg" alt={name} width='60px' style="opacity: {opp}" /> -->
     <img
+      id="id{id}"
       src="./assets/noun-gear-941149.svg"
       alt={name}
       width="60px"
@@ -20,8 +37,22 @@
     />
   </div>
   {name}
-  <br />
-  {gen} / {size}
+  <!-- <br />
+  {gen} / {size} -->
+  <Popover class="w-64 text-sm " title={name} triggeredBy="#id{id}">
+    {#if gen >= 0}
+      Generating: {gen} MW
+    {:else}
+      Pumping: {-gen} MW
+    {/if}
+    <br />Capacity: {size}
+    <br />Head: {head} m
+    <br />Turbines: {turbines}
+    <br />Turbine size: {turbineSize} MW
+    <br />Max Flow: {maxFlow} m^3/s
+    <br />Water Flow: {calcFlow} m^3/s
+    <br />Water Flow: {calcFlowML} ML/h
+  </Popover>
 </div>
 
 <style>
@@ -57,5 +88,4 @@
       transform: rotate(0deg);
     }
   }
-
 </style>
